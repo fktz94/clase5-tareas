59c3524d9f3af11d951c23cb1aa3de2782c311f2
fktz94 <106633973+fktz94@users.noreply.github.com>
2022-09-06 23:40:50 -0300

    Primera parte de tarea 4
    Resolución del promedio sobre el NodeList que nos dió los li con números.
    Se planteó la búsqueda del Math.min() pero falta leer documentación para entenderlo.

--- tarea4-clase5.js
+++ tarea4-clase5.js
@@ -6,7 +6,30 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
-function crearArray(){
-    const $numerosDeLaOl = document.querySelectorAll('ol li')
-    for (let i = 0; )
+function crearArray() {
+    let $numerosDeLaOl = document.querySelectorAll('ol > li')
+
+    function calcularPromedio() {
+        let contador = 0;
+        for (let i = 0; i < $numerosDeLaOl.length; i++) {
+            contador += Number($numerosDeLaOl[i].textContent);
+        }
+        // console.log(contador);
+        const promedio = contador / $numerosDeLaOl.length;
+        // console.log(promedio);
+        const $domPromedio = document.querySelector('#promedio')
+        $domPromedio.textContent = `El promedio es de ${promedio}`;
+    }
+    calcularPromedio();
+
+    function numeroPequenio() {
+        for (let i = 0; i < $numerosDeLaOl.length; i++) {
+            const numeros = $numerosDeLaOl[i];
+            let mathMin = Math.min(numeros);
+            console.log(mathMin);
+        }
+    }
+    numeroPequenio();
 }
+crearArray();
+
